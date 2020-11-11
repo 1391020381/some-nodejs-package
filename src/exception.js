ac565d0b6d149b35e49d603eae616b78713f5638
zhoujin <171226Q2>
2020-11-11 11:45:50 +0800

    demo

--- src/exception.js
+++ src/exception.js
@@ -1,19 +1,26 @@
 const path = require('path')
-const { transports } = require('winston')
 const winston = require('winston')
+const { transports } = require('winston')
+require('winston-daily-rotate-file')
 const logger = winston.createLogger({
     format:winston.format.simple(),
     transports:[
-        // new winston.transports.Console({
-        //     handleExceptions:true
-        // })
+        new winston.transports.Console({
+            handleExceptions:true
+        })
         
     ],
     exceptionHandlers:[
         new transports.File({
             filename:path.join(__dirname,'../logs/exceptions.log')
+        }),
+        new transports.DailyRotateFile({
+            filename: path.join(__dirname,"../logs/exceptionhandle-%DATE%.log"),
+            datePattern:'YYYY-MM-DD-HH',
+            zippedArchive:true,
+            maxSize:'20m',
+            maxFiles:'14d',
+            level:'error'
         })
     ]
 })
-console.log(xxxxxxx.a)
-throw new Error('Hello winston!')
