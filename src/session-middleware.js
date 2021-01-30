a1e7c96d7f7a76e3b4cce5a9b1401e02cc4253d8
zhoujin <1391020381@qq.com>
2021-01-30 12:25:58 +0800

    winston

--- src/session-middleware.js
+++
@@ -1,8 +0,0 @@
-
-const session = require('./session')
-const uuid = require('uuid')
-module.exports = function createRequestId (req,res,next){
-    const requestId = req.get['x-request-id'] || uuid.v1()
-   session.set('requestId',requestId)
-    next()
-}
--- src/session.js
+++
@@ -1,4 +0,0 @@
-const createNamespace  = require('cls-hooked').createNamespace
-const session = createNamespace('node-pc')
-
-module.exports = session
