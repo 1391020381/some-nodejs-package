46b0f22dd0c5e5ce6ce07f83fb96f86f91528f34
zhoujin <1391020381@qq.com>
2021-04-10 12:19:03 +0800

    base-type

---
+++ ts/sourceCode/ts-base/base-type.ts
@@ -0,0 +1,9 @@
+let isDone:boolean = false
+let decLiteral:number = 20
+let userName:string = 'bob'
+let list:number[] = [1,2,3]
+let list_1:Array<number> = [1,2,3]
+let x:[string,number] 
+x = ['hello',10]
+enum Color { Red,Green,Blue}
+let c:Color = Color.Green
